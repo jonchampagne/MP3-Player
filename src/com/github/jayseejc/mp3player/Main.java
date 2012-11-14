edc345827d6a39483f0fe4bde9af7de75bcbc7d6
Jon Champagne <jayseejc3191@gmail.com>
2012-11-14 16:21:42 -0500

    First commit

---
+++ src/com/github/jayseejc/mp3player/Main.java
@@ -0,0 +1,13 @@
+package com.github.jayseejc.mp3player;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey");
+	}
+
+}
